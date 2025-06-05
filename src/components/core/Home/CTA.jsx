e89243c396960e117b6141179470835e499eb14e
tanmayrane18 <tanmayrane51@gmail.com>
2025-06-05 17:06:56 +0530

    first draft of homepage completed

--- src/components/core/Home/CTA.jsx
+++ src/components/core/Home/CTA.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router";
+import image from '../../../assets/home/homeCta.jpg'
 
 const CTA = () => {
   return (
@@ -7,9 +8,9 @@ const CTA = () => {
       <div className=" h-[90vh] w-full relative flex items-end">
         <img
           className=" w-full h-full"
-          src="https://educateradiateelevate.org/wp-content/uploads/2023/04/pexels-mikhail-nilov-8923075-1-1024x682.jpg"
+          src={image}
         />
-        <div className=" absolute top-0 right-0 left-0 bottom-0 bg-[rgba(0,0,0,0.75)] flex flex-col justify-center items-center">
+        <div className=" absolute top-0 right-0 left-0 bottom-0 bg-[rgba(0,0,0,0.8)] flex flex-col justify-center items-center">
           <div className=" text-white flex flex-col px-10">
             <p className=" text-xl">
               Hard work is worthless for those that don't believe in themselves.
@@ -24,7 +25,7 @@ const CTA = () => {
           }}
           className=" translate-y-1/2 absolute w-[90%] translate-x-1/2 right-1/2 bg-purple-600 py-20 px-5 rounded-4xl flex flex-col justify-center items-center"
         >
-          <p className=" text-white text-lg lg:text-3xl text-center mb-10">
+          <p className=" text-white  md:text-2xl text-lg lg:text-3xl text-center mb-10">
             From Little Seeds to Mighty Blossoms <br className=" lg:hidden" /> —
             Join the Tulip Tots Family!
           </p>
@@ -35,7 +36,7 @@ const CTA = () => {
           </NavLink>
         </div>
       </div>
-      <div className=" h-80 w-full" />
+      <div className=" h-64 sm:h-80 w-full" />
     </>
   );
 };
