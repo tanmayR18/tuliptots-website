127324134b3adee297fc1e76a26a118929d04850
tanmayrane18 <tanmayrane51@gmail.com>
2026-02-08 10:24:12 +0530

    added protected route

--- src/screens/Auth/NewDashboard.jsx
+++ src/screens/Auth/NewDashboard.jsx
@@ -1,14 +1,27 @@
-import { SignedIn, UserButton } from "@clerk/clerk-react";
-import React from "react";
-import { Link } from "react-router";
+import { SignedIn, UserButton, useUser } from "@clerk/clerk-react";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router";
 
 const NewDashboard = () => {
+    const { isSignedIn } = useUser();
+
+  const navigate = useNavigate();
+    useEffect(() => {
+      if (!isSignedIn) {
+        navigate("/");
+      }
+    }, [isSignedIn, navigate]);
+
   return (
-    <div className=" font-kgPrimaryPenmanship bg-[#fafafa] py-32 lg:py-44 px-4 min-h-screen justify-center items-center">
+    <div className=" font-kgPrimaryPenmanship bg-[#fafafa] py-32 lg:py-44 min-h-screen justify-center items-center">
+      <div className=" w-[90%] mx-auto">
+        <div className=" border w-fit px-3 py-1 rounded-xl cursor-pointer" onClick={() => navigate(-1)}>
+          <h1 className=" text-xl">Back</h1>
+        </div>
+      </div>
+
       <div className=" flex relative justify-center items-center mx-auto lg:w-10/12">
-        <h1 className=" text-3xl md:text-4xl lg:text-5xl">
-          Admin Panel
-        </h1>
+        <h1 className=" text-3xl md:text-4xl lg:text-5xl">Admin Panel</h1>
 
         <div className=" absolute  top-1/2 -translate-y-1/2 right-0">
           <SignedIn>
